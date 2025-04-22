07d8b0c35d0d271b2600d4c045809f7ed2b12cdc
Lexyth <marfy@web.de>
2025-04-22 03:51:22 +0200

    (feat): Add type Key

---
+++ src/types/Key.ts
@@ -0,0 +1,70 @@
+type Alpha =
+  | "a"
+  | "b"
+  | "c"
+  | "d"
+  | "e"
+  | "f"
+  | "g"
+  | "h"
+  | "i"
+  | "j"
+  | "k"
+  | "l"
+  | "m"
+  | "n"
+  | "o"
+  | "p"
+  | "q"
+  | "r"
+  | "s"
+  | "t"
+  | "u"
+  | "v"
+  | "w"
+  | "x"
+  | "y"
+  | "z";
+
+type Numerical = "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9";
+
+type Punctuation =
+  | " "
+  | "!"
+  | '"'
+  | "#"
+  | "$"
+  | "%"
+  | "&"
+  | "'"
+  | "("
+  | ")"
+  | "*"
+  | "+"
+  | ","
+  | "-"
+  | "."
+  | "/"
+  | ":"
+  | ";"
+  | "<"
+  | "="
+  | ">"
+  | "?"
+  | "@"
+  | "["
+  | "\\"
+  | "]"
+  | "^"
+  | "_"
+  | "`"
+  | "{"
+  | "|"
+  | "}"
+  | "~";
+
+export type Key = Alpha | Numerical | Punctuation;
+
+export function isKey(key: string): key is Key {
+  return key.length === 1 && /^[a-z]$/i.test(key);
+}
